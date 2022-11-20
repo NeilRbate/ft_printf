22655bda5857353b55d5b3d370ecba4df3ade1af
Rbate <98659534+NeilRbate@users.noreply.github.com>
2022-11-20 07:41:35 +0100

    Add putnbr_base

--- srcs/ft.h
+++ srcs/ft.h
@@ -6,10 +6,16 @@
 /*   By: jbarbate <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 15:59:49 by jbarbate          #+#    #+#             */
-/*   Updated: 2022/11/18 16:13:30 by jbarbate         ###   ########.fr       */
+/*   Updated: 2022/11/20 07:01:06 by jbarbate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putnbr_fd(int n, int fd);
 size_t	ft_strlen(const char *s1);
 void	ft_putstr_fd(const char *s1, int fd);
 void	ft_putchar_fd(const char c, int fd);
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,25 +6,80 @@
 /*   By: jbarbate <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 09:57:21 by jbarbate          #+#    #+#             */
-/*   Updated: 2022/11/19 11:46:00 by jbarbate         ###   ########.fr       */
+/*   Updated: 2022/11/20 07:36:29 by jbarbate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdarg.h>
 #include "ft.h"
+
+int	base_valid(char *str)
+{
+	int		len;
+	int		i;
+	int		j;
+	char	stock;
+
+	i = 0;
+	j = 0;
+	stock = 0;
+	len = ft_strlen(str);
+	if (len < 2)
+		return (0);
+	while (j > len)
+	{
+		stock = str[j];
+		while (str[i])
+		{
+			if (stock == str[i] || stock == '+' || stock == '-')
+				return (0);
+			i++;
+		}
+		i = 0;
+		j++;
+	}
+	return (len);
+}
+
+void	ft_putnbr_base_fd(int nbr, char *base, int fd)
+{
+	int		base_size;
+	long	nbr2;
+
+	nbr2 = nbr;
+	base_size = base_valid(base);
+	if (base_size > 1)
+	{
+		if (nbr < 0)
+		{
+			ft_putchar_fd('-', fd);
+			nbr2 = -nbr2;
+		}
+		if (nbr2 < base_size)
+			ft_putchar_fd(base[nbr2], fd);
+		else
+		{
+			ft_putnbr_base_fd((nbr2 / base_size), base, fd);
+			ft_putnbr_base_fd((nbr2 % base_size), base, fd);
+		}
+	}
+}
 
 void	ft_print(const char format, va_list args)
 {
-	va_start(args, format);
 	if (format == '%')
-		return (ft_putchar_fd(%));
+		return (ft_putchar_fd('%', 1));
 	else if (format == 'c')
 		return (ft_putchar_fd(va_arg(args, int), 1));
 	else if (format == 's')
 		return (ft_putstr_fd(va_arg(args, char *), 1));
-	else if ((format == 'd' || format[i] == 'i')
-		return (ft_putnbr(va_arg(args, int), 1));
+	else if (format == 'd' || format == 'i')
+		return (ft_putnbr_fd(va_arg(args, int), 1));
+	else if (format == 'u')
+		return (ft_putnbr_fd(va_arg(args, unsigned int), 1));
+	else if (format == 'x')
+		return (ft_putnbr_base_fd(va_arg(args, int), "0123456789abcdef", 1));
+	else if (format == 'X')
+		return (ft_putnbr_base_fd(va_arg(args, int), "0123456789ABCDEF", 1));
 
 
 }
@@ -32,23 +87,25 @@ void	ft_print(const char format, va_list args)
 int	ft_printf(const char *format, ...)
 {
 	va_list args;
+	va_start(args, format);
+	int	i;
+
+	i = 0;
 	while (format[i])
 	{
-		ft_print(format[i], args);	
+		if (format[i++] == '%')
+			ft_print(format[i], args);
+		i++;
 	}
-	j += i;
 	va_end(args);
-	return (j);
+	return (0);
 }
-
-#include <stdio.h>
 
 int main()
 {
-	int i;
-	char *test = "Salut !";
-	char tes = 'A';
-	i = ft_printf("%s, ceci est une phrase, -> %c\n", test, tes);
-	printf("%d", i);
+	int i = 10;
+	int a = 155;
+	i = ft_printf("%X\n", a);
+	ft_printf("%i", i);
 	return (0);
 }
